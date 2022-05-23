05294e7ac1485f5d1a23d63fcddbb3a171036954
Sean Edwards <sean.edwards@teachingpersonnel.com>
2022-05-23 14:42:55 +0100

    Added in regster pages and change layout of home page moved bavbar to top

--- SeanProfileBlazor/Program.cs
+++ SeanProfileBlazor/Program.cs
@@ -8,7 +8,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddHttpClient<ITodoDataService, TodoDataService>(client => { client.BaseAddress = new Uri("https://localhost:1989/"); });
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
